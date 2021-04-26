1ddb0c1344f3594fdad84d6aa34f4ae890a21e8b
Zakarialabib <58374576+Zakarialabib@users.noreply.github.com>
2021-04-26 12:31:40 +0000

    some changes

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,19 +1,5 @@
-@extends('layouts.error')
+@extends('errors::illustrated-layout')
 
-@section('page-title') 404 @endsection
-
-@section('content')
-
-    <section class="flexbox-container">
-        <div class="col-12 d-flex align-items-center justify-content-center">
-            <div class="col-md-4 col-10 p-0">
-                <div class="card-header bg-transparent border-0">
-                    <h2 class="error-code text-center mb-2">404</h2>
-                    <h3 class="text-uppercase text-center">Page Not Found !</h3>
-                    <a href="{{url('/')}}" class="btn btn-primary btn-block"><i class="ft-arrow-left"></i> GO BACK</a>
-                </div>
-            </div>
-        </div>
-    </section>
-
-@endsection
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message', __('Not Found'))
